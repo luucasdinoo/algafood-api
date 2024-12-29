ce5c83bc98e972cfd43871a623e1f09b6a5176a5
Lucas Bernadino <luucasdinoo@gmail.com>
2024-12-28 23:20:14 -0300

    Inicial Commit

---
+++ src/test/java/com/dino/algafood/api/AlgafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dino.algafood.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
