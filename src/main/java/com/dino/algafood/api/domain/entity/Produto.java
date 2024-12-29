1d0af864ded15245333dfba365bd59f963b14580
Lucas Bernadino <luucasdinoo@gmail.com>
2024-12-29 19:43:33 -0300

    feat usuario-produto: entities

---
+++ src/main/java/com/dino/algafood/api/domain/entity/Produto.java
@@ -0,0 +1,34 @@
+package com.dino.algafood.api.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Produto {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private boolean ativo;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurante_id", nullable = false)
+    private Restaurante restaurante;
+}
