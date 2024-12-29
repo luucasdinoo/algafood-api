602807d5a311e7277a4df18cc7527708020a7587
Lucas Bernadino <luucasdinoo@gmail.com>
2024-12-29 18:20:34 -0300

    feat restaurante: entity, controller, service, repository

---
+++ src/main/java/com/dino/algafood/api/api/controller/RestauranteController.java
@@ -0,0 +1,86 @@
+package com.dino.algafood.api.api.controller;
+
+import com.dino.algafood.api.domain.entity.Restaurante;
+import com.dino.algafood.api.domain.service.RestauranteService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/restaurantes", produces = MediaType.APPLICATION_JSON_VALUE)
+public class RestauranteController {
+
+    @Autowired
+    private RestauranteService restauranteService;
+
+    @GetMapping
+    public ResponseEntity<List<Restaurante>> listar() {
+        List<Restaurante> restaurantes = restauranteService.listar();
+        return ResponseEntity.ok(restaurantes);
+    }
+
+    @GetMapping("/{restauranteId}")
+    @ResponseStatus(HttpStatus.OK)
+    public Restaurante buscar(@PathVariable Long restauranteId) {
+        return restauranteService.buscarOuFalhar(restauranteId);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Restaurante adicionar(@RequestBody @Valid Restaurante restaurante) {
+        return restauranteService.salvar(restaurante);
+    }
+
+    @PutMapping("/{restauranteId}")
+    public Restaurante atualizar(@RequestBody Restaurante restaurante, @PathVariable Long restauranteId) {
+        Restaurante restauranteAtual = restauranteService.buscarOuFalhar(restauranteId);
+        BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formaPagamentos", "endereco", "dataCadastro");
+        return restauranteService.salvar(restauranteAtual);
+    }
+
+    @PatchMapping("/{restauranteId}")
+    public Restaurante atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos, HttpServletRequest request){
+        Restaurante restauranteAtual = restauranteService.buscarOuFalhar(restauranteId);
+        merge(campos, restauranteAtual, request);
+        return atualizar(restauranteAtual, restauranteId);
+    }
+
+    private void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestito, HttpServletRequest request){
+        ServletServerHttpRequest servletServerHttpRequest = new ServletServerHttpRequest(request);
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+
+            Restaurante restauranteOrigem = objectMapper.convertValue(camposOrigem, Restaurante.class);
+
+            camposOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+                Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+                field.setAccessible(true);
+
+                Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
+                ReflectionUtils.setField(field, restauranteDestito, novoValor);
+            });
+        }catch (IllegalArgumentException e){
+            Throwable rootCause = ExceptionUtils.getRootCause(e);
+            throw  new HttpMessageNotReadableException(e.getMessage(), rootCause, servletServerHttpRequest);
+        }
+    }
+}
---
+++ src/main/java/com/dino/algafood/api/domain/service/RestauranteService.java
@@ -0,0 +1,38 @@
+package com.dino.algafood.api.domain.service;
+
+import com.dino.algafood.api.domain.entity.Cozinha;
+import com.dino.algafood.api.domain.entity.Restaurante;
+import com.dino.algafood.api.domain.exception.RestauranteNaoEncontradoException;
+import com.dino.algafood.api.domain.repository.RestauranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class RestauranteService {
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @Autowired
+    private CadastroCozinhaService cadastroCozinhaService;
+
+    public List<Restaurante> listar() {
+        return restauranteRepository.findAll();
+    }
+
+    public Restaurante buscarOuFalhar(Long id){
+        return restauranteRepository.findById(id)
+                .orElseThrow(() -> new RestauranteNaoEncontradoException(id));
+    }
+
+    public Restaurante salvar(Restaurante restaurante){
+        Long cozinhaId = restaurante.getCozinha().getId();
+
+        Cozinha cozinha = cadastroCozinhaService.buscarOuFalhar(cozinhaId);
+        restaurante.setCozinha(cozinha);
+        return restauranteRepository.save(restaurante);
+    }
+}
